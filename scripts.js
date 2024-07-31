0535a4166d1be64fd05e112ae6c446212591f6ab
Mork <157231@ishweb.nl>
2024-07-31 19:26:26 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,27 @@
+let keySequence = 'Ugugula';
+let currentInput = '';
+
+document.addEventListener('keydown', function(event) {
+    // Append the key pressed to the current input
+    currentInput += event.key;
+
+    // Check if the current input matches the key sequence
+    if (currentInput.toLowerCase().includes(keySequence.toLowerCase())) {
+        document.querySelector('.menu-container').classList.add('red-bg');
+        const secretMessage = document.createElement('div');
+        secretMessage.className = 'secret-message';
+        secretMessage.innerText = 'Welcome to the underworld. Have fun here bro :P';
+        document.body.insertBefore(secretMessage, document.body.firstChild);
+        // Reset the current input
+        currentInput = '';
+    }
+
+    // Reset current input if it exceeds the key sequence length
+    if (currentInput.length > keySequence.length) {
+        currentInput = currentInput.slice(-keySequence.length);
+    }
+});
+
 function navigate(page) {
     window.location.href = page;
 }
