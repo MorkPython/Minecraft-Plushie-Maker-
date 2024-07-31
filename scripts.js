6862b9912f78071b32cb4c8942eb9f57040e4cfb
Mork <157231@ishweb.nl>
2024-07-31 19:05:50 +0200

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,15 @@
+function navigate(page) {
+    window.location.href = page;
+}
+
+function showOptions() {
+    document.getElementById('skin-tricks-menu').classList.add('active');
+}
+
+function closeOptions() {
+    document.getElementById('skin-tricks-menu').classList.remove('active');
+}
+
+function quitGame() {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
